e37e79824874a824564e3bc3f6a2dc12594530b2
Mariana <mariana.c.novaes22@gmail.com>
2022-07-03 17:06:21 -0300

    Adicionar arquivos de projeto.

---
+++ 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i, num;
+            for (i = 1; i <= 100; i++)
+            {
+
+                num = i % 2;
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
